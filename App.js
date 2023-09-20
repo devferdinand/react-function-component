f68d90c0b244593d44d5c33c3f12da064e11e75d
Ferdinand Lukito <devferdinandlukito@gmail.com>
2023-09-20 17:00:56 -0400

    define top level component App. Call instance of Card component for each comment and pass props

--- App.js
+++ App.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import {comments} from './commentData'
+
+// define App component
+function App(){
+    // map over comments array
+    comments.map(comment => {
+        <Card commentObject = {comment}/>
+    });
+};
