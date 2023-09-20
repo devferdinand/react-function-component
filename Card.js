9d46a8c9cf2d44b6e4b2017ecad6a68705fea557
Ferdinand Lukito <devferdinandlukito@gmail.com>
2023-09-20 17:08:33 -0400

    define Card component and pass props

--- Card.js
+++ Card.js
@@ -1,1 +1,15 @@
 import React from 'react';
+import Header from './Header';
+import Body from './Body';
+
+// define Card component
+function Card(props){
+    return (
+        <div>
+            <Header profileImg={props.commentObject.profileImg} username={props.commentObject.username}/>;
+            <Body comment={props.commentObject.comment}/>;
+        </div>
+    );
+};
+
+export default Card;
